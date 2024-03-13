7a3b41cace91f0a87ed3b703a682e40f1b303699
New-Tong <1248449622@qq.com>
2024-03-13 18:00:06 +0800

    定义模型

---
+++ bookmanager/book/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Book(models.Model):
+    # id 自动生成
+    name = models.CharField(max_length=10)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
