c48c4a5458458662f3b71d8b684b4c038c6158ba
New-Tong <1248449622@qq.com>
2024-03-14 11:16:55 +0800

    settings.py修改

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--p(a2=!yaxw+@a!0zs!c335$*me47_0^0)xbts+x0-9trt)^(-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
